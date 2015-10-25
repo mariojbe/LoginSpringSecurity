dab83a9c1cbe42ce7e2e55bce5a570d7fbeed8b3
mariojbe <mariojbe@hotmail.com>
2015-10-24 23:58:23 -0200

    Revizando login.js e AS.java

--- src/main/java/br/org/abmnet/security/AuthSuccessHandler.java
+++ src/main/java/br/org/abmnet/security/AuthSuccessHandler.java
@@ -1,7 +1,11 @@
 package br.org.abmnet.security;
 
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -21,9 +25,21 @@ public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHa
         String targetUrl = "";
         if (role.contains("ROLE_USER")) {
             targetUrl = "/pages/main.jsp";
+
         } else if (role.contains("ROLE_ADMIN")) {
             targetUrl = "/pages/admin.jsp";
         }
         return targetUrl;
     }
+/*
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(response);
+        Writer out = responseWrapper.getWriter();
+        //out.write("{success:true, targetUrl : \'" + targetUrl + "\'}");
+        out.write("{success:true}");
+        out.close();
+    }
+    */
+
 }
